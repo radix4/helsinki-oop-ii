5acb3d897a6cf1d90938e87c98803598bc445a19
Thang Cao <shiyoline@gmail.com>
2020-06-13 11:20:31 -0700

    Implemented class 'Hand'.

---
+++ week8-week8_15.SortedCards/src/Hand.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Hand implements Comparable<Hand>{
+    private List<Card> cards;
+    private int totalValues;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+        this.totalValues = 0;
+    }
+
+    public void add(Card card) {
+        if (cards.contains(card)) {
+            return;
+        }
+        totalValues += card.getValue();
+        cards.add(card);
+    }
+
+    public int getTotalValues() {
+        return totalValues;
+    }
+
+    public void sort() {
+        Collections.sort(cards);
+    }
+
+    public void print() {
+        for (Card c : cards) {
+            System.out.println(c);
+        }
+    }
+
+    @Override
+    public int compareTo(Hand o) {
+        return totalValues - o.getTotalValues();
+    }
+}
--- week8-week8_15.SortedCards/src/Main.java
+++ week8-week8_15.SortedCards/src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/** This program
+/** This program sorts a deck of card against different criteria.
  * @author  Thang Cao
  * @since   06/13/2020
  * */
@@ -10,13 +10,29 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Card> cards = new ArrayList<>();
-        cards.add(new Card(7,1));
-        cards.add(new Card(6,2));
-        cards.add(new Card(5,3));
+        Hand hand1 = new Hand();
 
-        Collections.sort(cards);
+        hand1.add( new Card(2, Card.SPADES) );
+        hand1.add( new Card(14, Card.CLUBS) );
+        hand1.add( new Card(12, Card.HEARTS) );
+        hand1.add( new Card(2, Card.CLUBS) );
 
-        System.out.println(cards);
+        Hand hand2 = new Hand();
+
+        hand2.add( new Card(11, Card.DIAMONDS) );
+        hand2.add( new Card(11, Card.CLUBS) );
+        hand2.add( new Card(11, Card.HEARTS) );
+
+        int comparison = hand1.compareTo(hand2);
+
+        if ( comparison < 0 ) {
+            System.out.println("the most valuable hand contains the cards");
+            hand2.print();
+        } else if ( comparison > 0 ){
+            System.out.println("the most valuable hand contains the cards");
+            hand1.print();
+        } else {
+            System.out.println("the hands are equally valuable");
+        }
     }
 }
