d71527e4cc8f7bd5b80080dec30c9dc026c1ee98
Thang Cao <shiyoline@gmail.com>
2020-06-15 23:10:26 -0700

    Exercise 17 completed.

---
+++ week9-week9_17.FirstPackages/src/mooc/Main.java
@@ -0,0 +1,13 @@
+package mooc;
+
+import mooc.logic.ApplicationLogic;
+import mooc.ui.UserInterface;
+import mooc.ui.TextUserInterface;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserInterface ui = new TextUserInterface();
+        new ApplicationLogic(ui).execute(3);
+    }
+}
