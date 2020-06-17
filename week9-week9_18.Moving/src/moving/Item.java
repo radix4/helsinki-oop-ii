60ca53e67971def3f796ea047950aa64c12b46b7
Thang Cao <shiyoline@gmail.com>
2020-06-16 22:00:52 -0700

    Added class Item.

--- week9-week9_18.Moving/src/moving/Item.java
+++ week9-week9_18.Moving/src/moving/Item.java
@@ -1,7 +1,26 @@
 package moving;
 
-public class Item {
+public class Item implements Thing, Comparable<Item> {
     private String name;
     private int volume;
 
+    public Item(String name, int volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    @Override
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + volume + " dm^3)";
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        return volume - o.getVolume();
+    }
 }
--- week9-week9_18.Moving/src/moving/Main.java
+++ week9-week9_18.Moving/src/moving/Main.java
@@ -1,8 +1,25 @@
+/**
+ * This program puts things into boxes, then boxes into packers in an efficient way.
+ * @author  Thang Cao
+ * @since   06/16/2020
+ * */
+
 package moving;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        // test your program here
+        Thing item = new Item("toothbrash", 2);
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("passport", 2));
+        items.add(new Item("toothbrash", 1));
+        items.add(new Item("circular saw", 100));
+
+        Collections.sort(items);
+        System.out.println(items);
     }
 }
