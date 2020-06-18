d63f0426be430e51066e0c8472801b41c298ed21
Thang Cao <shiyoline@gmail.com>
2020-06-17 21:45:53 -0700

    Exercise 19.1 completed.

--- week9-week9_19.MethodArgumentValidation/src/validation/Person.java
+++ week9-week9_19.MethodArgumentValidation/src/validation/Person.java
@@ -7,6 +7,14 @@ public class Person {
 
     public Person(String name, int age) {
 
+        if (name == null || name.equals("") || name.length() > 40) {
+            throw new IllegalArgumentException("Blabla!");
+        }
+
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Blop!");
+        }
+
         this.name = name;
         this.age = age;
     }
