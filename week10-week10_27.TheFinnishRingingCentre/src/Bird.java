92eb4217cc2423c40e654197b389f5304a185b2c
Thang Cao <shiyoline@gmail.com>
2020-06-25 08:39:43 -0700

    Exercise 27.1 completed.

--- week10-week10_27.TheFinnishRingingCentre/src/Bird.java
+++ week10-week10_27.TheFinnishRingingCentre/src/Bird.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class Bird {
 
@@ -11,10 +12,57 @@ public class Bird {
         this.ringingYear = ringingYear;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        if (this == o) {
+            return true;
+        }
+
+        if (this.getClass() == o.getClass()){
+            return true;
+        }
+
+        Bird compared = (Bird) o;
+
+        return latinName.equals(compared.getLatinName()) && ringingYear == compared.ringingYear;
+    }
+
+    public String getLatinName() {
+        return latinName;
+    }
+
+    public int getRingingYear() {
+        return ringingYear;
+    }
+
+    @Override
+    public int hashCode() {
+       if (latinName == null) {
+           return ringingYear;
+       }
+
+       return latinName.hashCode() + ringingYear;
+    }
 
     @Override
     public String toString() {
         return this.latinName + " (" + this.ringingYear + ")";
+    }
+
+    public static void main(String[] args) {
+        Bird bird1 = new Bird("Rose Starling", "Sturnus roseus", 2012);
+        Bird bird2 = new Bird("Rose-Coloured Starling", "Sturnus roseus", 2012);
+        Bird bird3 = new Bird("Hooded Crow", "Corvus corone cornix", 2012);
+        Bird bird4 = new Bird("Rose-Coloured Pastor", "Sturnus roseus", 2000);
+
+        System.out.println( bird1.equals(bird2));   // same Latin name and same observation year: they are the same bird
+        System.out.println( bird1.equals(bird3));   // different Latin name: they are not the same bird
+        System.out.println( bird1.equals(bird4));   // different observation year: not the same bird
+        System.out.println( bird1.hashCode()==bird2.hashCode() );
     }
 }
 
