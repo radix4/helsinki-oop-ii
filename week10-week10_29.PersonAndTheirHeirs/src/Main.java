5b89ff68c62a4b89f421b35997c5ac7f2186d981
Thang Cao <shiyoline@gmail.com>
2020-06-27 09:13:34 -0700

    Exercise 29.2 completed.

--- week10-week10_29.PersonAndTheirHeirs/src/Main.java
+++ week10-week10_29.PersonAndTheirHeirs/src/Main.java
@@ -1,4 +1,5 @@
 import people.Person;
+import people.Student;
 
 /**
  * This program demonstrates inheritance feature.
@@ -9,9 +10,10 @@ import people.Person;
 public class Main {
 
     public static void main(String[] args) {
-        Person pekka = new Person("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa");
-        Person esko = new Person("Esko Ukkonen", "Mannerheimintie Street 15 00100 Helsinki");
-        System.out.println(pekka);
-        System.out.println(esko);
+        Student olli = new Student("Olli", "Ida Albergintie Street 1 00400 Helsinki");
+        System.out.println(olli);
+        System.out.println("credits " + olli.credits());
+        olli.study();
+        System.out.println("credits "+ olli.credits());
     }
 }
---
+++ week10-week10_29.PersonAndTheirHeirs/src/people/Student.java
@@ -0,0 +1,19 @@
+package people;
+
+/** A student "is a" person --> extends (inheritance) */
+public class Student extends Person {
+    private int credits;
+
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = 0;
+    }
+
+    public int credits(){
+        return credits;
+    }
+
+    public void study(){
+        credits++;
+    }
+}
