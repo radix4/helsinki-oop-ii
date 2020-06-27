eaf66bc8b47c7c5fc16b40974a1743daa84648fb
Thang Cao <shiyoline@gmail.com>
2020-06-27 09:06:59 -0700

    Exercise 29.1 completed.

--- week10-week10_29.PersonAndTheirHeirs/src/Main.java
+++ week10-week10_29.PersonAndTheirHeirs/src/Main.java
@@ -1,8 +1,17 @@
+import people.Person;
 
+/**
+ * This program demonstrates inheritance feature.
+ * @author  Thang Cao
+ * @since   06/27/2020
+ * */
 
 public class Main {
 
     public static void main(String[] args) {
-        // test your code here
+        Person pekka = new Person("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa");
+        Person esko = new Person("Esko Ukkonen", "Mannerheimintie Street 15 00100 Helsinki");
+        System.out.println(pekka);
+        System.out.println(esko);
     }
 }
---
+++ week10-week10_29.PersonAndTheirHeirs/src/people/Person.java
@@ -0,0 +1,16 @@
+package people;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\n  " + address;
+    }
+}
