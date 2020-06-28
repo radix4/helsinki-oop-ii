2f456939bba26d3cbee3058503501ba4df479b39
Thang Cao <shiyoline@gmail.com>
2020-06-28 12:32:37 -0700

    Exercise 30.4 completed.

--- week10-week10_30.Container/src/containers/ContainerHistory.java
+++ week10-week10_30.Container/src/containers/ContainerHistory.java
@@ -19,7 +19,26 @@ public class ContainerHistory {
     public void reset() {
         history.clear();
     }
+    public double minValue() {
+        return Collections.min(history);
+    }
 
+    public double maxValue() {
+        return Collections.max(history);
+    }
+
+    public double average() {
+        if (history.isEmpty()) {
+            return 0;
+        }
+
+        double sum = 0;
+        for (double situation : history) {
+            sum += situation;
+        }
+
+        return sum / history.size();
+    }
 
     @Override
     public String toString() {
