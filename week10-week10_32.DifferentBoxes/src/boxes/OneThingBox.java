bcaa86c1217663d7958dd5eae27309a6cc680055
Thang Cao <shiyoline@gmail.com>
2020-06-30 10:02:12 -0700

    Exercise 32 completed.

---
+++ week10-week10_32.DifferentBoxes/src/boxes/OneThingBox.java
@@ -0,0 +1,23 @@
+package boxes;
+
+public class OneThingBox extends Box {
+
+    private Thing thing;
+
+    public OneThingBox() {
+    }
+
+    @Override
+    public void add(Thing thing) {
+        if (this.thing != null) {
+            return;
+        }
+
+        this.thing = thing;
+    }
+
+    @Override
+    public boolean isInTheBox(Thing thing) {
+        return this.thing != null && this.thing.equals(thing);
+    }
+}
