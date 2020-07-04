0ae6f8999a99baca1026ac45100a9e62887ada41
Thang Cao <shiyoline@gmail.com>
2020-07-03 19:25:05 -0700

    Exercise 35.4 completed.

--- week11-week11_35.TwoDirectionDictionary/src/dictionary/Main.java
+++ week11-week11_35.TwoDirectionDictionary/src/dictionary/Main.java
@@ -8,17 +8,11 @@ package dictionary;
 
 public class Main {
     public static void main(String[] args) {
-        MindfulDictionary dict = new MindfulDictionary();
-        dict.add("apina", "monkey");
-        dict.add("banaani", "banana");
-        dict.add("ohjelmointi", "programming");
-        dict.remove("apina");
-        dict.remove("banana");
+        MindfulDictionary dict = new MindfulDictionary("src/words.txt");
+        dict.load();
 
         System.out.println( dict.translate("apina") );
-        System.out.println( dict.translate("monkey") );
-        System.out.println( dict.translate("banana") );
-        System.out.println( dict.translate("banaani") );
         System.out.println( dict.translate("ohjelmointi") );
+        System.out.println( dict.translate("alla oleva") );
     }
 }
--- week11-week11_35.TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
+++ week11-week11_35.TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
@@ -1,13 +1,21 @@
 package dictionary;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class MindfulDictionary {
     private Map<String, String> words;
+    private String file;
 
     public MindfulDictionary() {
         words = new HashMap<String, String>();
+    }
+
+    public MindfulDictionary(String file) {
+        this();
+        this.file = file;
     }
 
     public void add(String word, String translation) {
@@ -24,9 +32,34 @@ public class MindfulDictionary {
     }
 
     public void remove(String word) {
+        if (!words.containsKey(word)) {
+            return;
+        }
+
         String translation = translate(word);
 
         words.remove(word);
         words.remove(translation);
+    }
+
+    public boolean load() {
+        try {
+            Scanner reader = new Scanner(new File(file));
+            readFileIntoDictionary(reader);
+            reader.close();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void readFileIntoDictionary(Scanner reader) {
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            String[] parts = line.split(":");
+
+            words.put(parts[0], parts[1]);
+            words.put(parts[1], parts[0]);
+        }
     }
 }
