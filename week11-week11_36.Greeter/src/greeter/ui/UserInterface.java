728d1f76ae1f87622aca1e8797508eba7513dc30
Thang Cao <shiyoline@gmail.com>
2020-07-04 22:04:34 -0700

    Exercise 36 completed.

--- week11-week11_36.Greeter/src/greeter/ui/UserInterface.java
+++ week11-week11_36.Greeter/src/greeter/ui/UserInterface.java
@@ -12,9 +12,20 @@ public class UserInterface implements Runnable {
 
     @Override
     public void run() {
+        frame = new JFrame("Swing on");
+        frame.setPreferredSize(new Dimension(400, 200));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     private void createComponents(Container container) {
+        JLabel messageLabel = new JLabel("Hi!");
+        container.add(messageLabel);
     }
 
     public JFrame getFrame() {
