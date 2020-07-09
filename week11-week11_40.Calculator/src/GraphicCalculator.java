9f152722403a96fa0c16eda789b430b52b7ae434
Thang Cao <shiyoline@gmail.com>
2020-07-08 21:27:10 -0700

    Exercise 40.1 completed.

--- week11-week11_40.Calculator/src/GraphicCalculator.java
+++ week11-week11_40.Calculator/src/GraphicCalculator.java
@@ -1,15 +1,39 @@
 
-import java.awt.Container;
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class GraphicCalculator implements Runnable {
     private JFrame frame;
 
     @Override
     public void run() {
+        frame = new JFrame("Calculator");
+        frame.setPreferredSize(new Dimension(300, 150));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     private void createComponents(Container container) {
+        frame.setLayout(new GridLayout(3, 1));
+        JTextField resultField = new JTextField("0");
+        container.add(resultField);
+        resultField.setEnabled(false);
+        JTextField inputField = new JTextField("");
+        container.add(inputField);
+
+        JButton plus = new JButton("+");
+        JButton minus = new JButton("-");
+        JButton reset = new JButton("Z");
+
+        JPanel panel = new JPanel(new GridLayout(1, 3));
+        panel.add(plus);
+        panel.add(minus);
+        panel.add(reset);
+        container.add(panel);
     }
 
     public JFrame getFrame() {
--- week11-week11_40.Calculator/src/Main.java
+++ week11-week11_40.Calculator/src/Main.java
@@ -1,4 +1,8 @@
-
+/**
+ * This program creates a simple UI calculator.
+ * @author  Thang Cao
+ * @since   07/08/2020
+ * */
 
 import javax.swing.SwingUtilities;
 
