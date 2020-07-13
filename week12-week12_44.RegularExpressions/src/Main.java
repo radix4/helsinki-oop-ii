4f5fc3bcad27219ffc001c8389fd38c4cd1da45a
Thang Cao <shiyoline@gmail.com>
2020-07-12 20:58:43 -0700

    exercise 44.1 completd.

--- week12-week12_44.RegularExpressions/src/Main.java
+++ week12-week12_44.RegularExpressions/src/Main.java
@@ -1,5 +1,14 @@
+/**
+ * This program demonstrates regular expressions in Java.
+ * @author  Thang Cao
+ * @since   07/12/2020
+ * */
 
 public class Main {
+
+    public static boolean isAWeekDay(String string) {
+        return string.matches("mon|tue|wed|thu|fri|sat|sun");
+    }
 
     public static void main(String[] args) {
         // write test code here
