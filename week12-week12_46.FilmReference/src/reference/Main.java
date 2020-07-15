2860d19d15b628076f036fee22743921638bac72
Thang Cao <shiyoline@gmail.com>
2020-07-14 19:31:02 -0700

    Exercise 46 completed.

--- week12-week12_46.FilmReference/src/reference/Main.java
+++ week12-week12_46.FilmReference/src/reference/Main.java
@@ -1,9 +1,43 @@
+/**
+ * This program recommends films to users.
+ * @author  Thang Cao
+ * @since   07/14/2020
+ * */
+
 package reference;
 
+
+import reference.comparator.FilmComparator;
+import reference.comparator.PersonComparator;
+import reference.domain.Film;
+import reference.domain.Person;
+import reference.domain.Rating;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Test your code here
+        RatingRegister ratings = new RatingRegister();
+
+        Film goneWithTheWind = new Film("Gone with the Wind");
+        Film theBridgesOfMadisonCounty = new Film("The Bridges of Madison County");
+        Film eraserhead = new Film("Eraserhead");
+
+        Person matti = new Person("Matti");
+        Person pekka = new Person("Pekka");
+        Person mikke = new Person("Mikke");
+
+        ratings.addRating(matti, goneWithTheWind, Rating.BAD);
+        ratings.addRating(matti, theBridgesOfMadisonCounty, Rating.GOOD);
+        ratings.addRating(matti, eraserhead, Rating.FINE);
+
+        ratings.addRating(pekka, goneWithTheWind, Rating.FINE);
+        ratings.addRating(pekka, theBridgesOfMadisonCounty, Rating.BAD);
+        ratings.addRating(pekka, eraserhead, Rating.MEDIOCRE);
+
+        Reference ref = new Reference(ratings);
+        Film recommended = ref.recommendFilm(mikke);
+        System.out.println("The film recommended to Michael is: " + recommended);
     }
 }
