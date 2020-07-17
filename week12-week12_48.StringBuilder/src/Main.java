f98697c380c8a89a398333cc22ae18947bbdb97e
Thang Cao <shiyoline@gmail.com>
2020-07-16 18:35:15 -0700

    Exercise 48 completed.

--- week12-week12_48.StringBuilder/src/Main.java
+++ week12-week12_48.StringBuilder/src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * This program demonstrates the benefit of StringBuilder method.
+ * @author  Thang Cao
+ * @since   07/16/2020
+ * */
 
 public class Main {
 
@@ -7,6 +12,21 @@ public class Main {
     }
 
     public static String build(int[] t) {
-        return "{\n}\n";
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+
+        for (int i = 0; i < t.length; i++) {
+            if (i % 4 == 0) {
+                sb.append("\n ");
+            }
+
+            sb.append(t[i]);
+            if (i != t.length - 1) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append("\n}");
+        return sb.toString();
     }
 }
