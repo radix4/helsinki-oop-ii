f8fb7fa2baccc8c424142788dc828c33ccafbe89
Thang Cao <shiyoline@gmail.com>
2020-07-18 20:58:45 -0700

    Exercise 50 completed.

--- week12-week12_50.CourseFeedback/src/Main.java
+++ week12-week12_50.CourseFeedback/src/Main.java
@@ -1,10 +1,19 @@
+/**
+ * This course is officially over! Thank you for the materials!
+ * @author  Thang Cao
+ * @since   07/18/2020
+ * */
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("" + 
-                "Course is over!\n" + 
-                "After you have submitted this exercise, remember to give us " + 
+        System.out.println("" +
+                "Course is over!\n" +
+                "After you have submitted this exercise, remember to give us " +
                 "some feedback about this course\n");
+
+        System.out.println("Feedback: I really enjoyed the materials presented," +
+                "some were very well coded and effective" +
+                "thank you for the exercises, I definitely learned a lot");
     }
 }
